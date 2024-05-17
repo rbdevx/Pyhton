fc41ff5db33e2d4bea1cdbfb8a7dc1645596b628
Radek Berner <rbdevx@gmail.com>
2024-05-17 16:11:11 +0200

    najnowsza wersja z strip title i split

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 # pierwszy kod w pythonie
-name = input("Whats your name? ")
-print(f"hello {name}")
-#dodany komentarz
+name = input("Whats your name? ").strip().title()
+first, last = name.split(" ")
+print(f"hello {first}")
